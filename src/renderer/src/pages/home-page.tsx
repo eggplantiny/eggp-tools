42aeeddb7c8187bb224441dd9714eda9c776fe1b
eggplantiny <eggplantiny@gmail.com>
2024-11-06 13:48:27 +0900

    test

--- src/renderer/src/pages/home-page.tsx
+++ src/renderer/src/pages/home-page.tsx
@@ -1,16 +1,9 @@
 import { PageRoot } from '@/components/ui/layout'
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 
 export function HomePage() {
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    navigate('/color-tool')
-  }, [])
   return (
     <PageRoot>
-      <h1>Home Page</h1>
+      <div />
     </PageRoot>
   )
 }
